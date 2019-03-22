c35379913c393387cec9492eeecb67e32c188474
fabio2019 <48735512+fabio2019@users.noreply.github.com>
2019-03-22 15:30:59 -0300

    formulario con estilos incorporados

--- Controlador/Formulario.php
+++ Controlador/Formulario.php
@@ -3,13 +3,120 @@ session_start();
 if(!isset($_SESSION['usuario'])){
 	header('location: index.php');
 }
-require('../vista/vistaAgenda.php');
+
 ?>
 <html>
 	<head>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="../css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="../css/style.min.css" rel="stylesheet">
+
+  <link href="../csscomp/estilos.css" rel="stylesheet">
+
+  <script src="../csscomp/sweetalert.min.js"></script>
+    <script src="../csscomp/alertas.js"></script>
+    <link rel="stylesheet" type="text/css" href="../csscomp/sweetalert.css">
+
+  <style type="text/css">
+    html,
+    body,
+    header,
+    .view {
+      height: 100%;
+    }
+
+    @media (max-width: 740px) {
+      html,
+      body,
+      header,
+      .view {
+        height: 1000px;
+      }
+    }
+
+    @media (min-width: 800px) and (max-width: 850px) {
+      html,
+      body,
+      header,
+      .view {
+        height: 650px;
+      }
+    }
+    @media (min-width: 800px) and (max-width: 850px) {
+              .navbar:not(.top-nav-collapse) {
+                  background: #1C2331!important;
+              }
+          }
+  </style>
 		<title>Formulario</title>
 	</head>
 	<body>
+		   <!-- Navbar -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+    <div class="container">
+
+      <!-- Brand -->
+      <a class="navbar-brand" href="#" >
+        <strong>SOFTRAIN</strong>
+      </a>
+
+      <!-- Collapse -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Links -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <!-- Left -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">	
+						Area de Trabajo
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <!--  <li class="nav-item">
+            <a class="nav-link" href="https://mdbootstrap.com/docs/jquery/" target="_blank">Registros</a>
+          </li>-->
+         
+          
+        </ul>
+
+        <!-- Right -->
+        <ul class="navbar-nav nav-flex-icons">
+          
+          <li class="nav-item">
+            <a href="logout.php" 
+							target="_blank">
+							<button type="button" class="btn btn-orange btn-sm">
+							Cerrar Sesion
+				</button>
+            </a>
+          </li>
+        </ul>
+		
+      </div>
+
+    </div>
+  </nav>
+  <!-- Navbar -->
+
+  
+		 <br><br><br>
+		 
+          <?php 
+		  require('../vista/vistaAgenda.php')
+		  ?>
+
+  
+  <!-- Full Page Intro -->
+
 		<form method="post" action="enviar.php">
 		<fieldset>			
 			<legend>Ingrese la informacion de la carga</legend>
@@ -62,7 +169,7 @@ require('../vista/vistaAgenda.php');
 					<label>Fecha devolucion:
 				</td>
 				<td>
-					<input type="date" name="fechaDev" />
+					<input type="date" name="fechaDev" readonly="readonly"/>
 					</label>
 				</td>					
 			</tr>
@@ -211,5 +318,18 @@ require('../vista/vistaAgenda.php');
 		</table>
 		</fieldset>
 		</form>
+		
+		<script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="../js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="../js/mdb.min.js"></script>
+  <!-- Initializations -->
+  <script type="text/javascript">
+    // Animations initialization
+    new WOW().init();
+  </script>
 	</body>
 </html>
