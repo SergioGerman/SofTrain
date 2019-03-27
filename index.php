67627d13a3af90d34e6bab5873b4a543fd240c51
Sergio Calle Luna <calleluna.sergiogerman@gmail.com>
2019-03-27 15:51:20 -0400

    index

--- index.php
+++ index.php
@@ -1,68 +1,70 @@
 ﻿<?php require('modelo/conexion.php'); ?>
 <html>
 	<head>
-		<title>Login</title>
+	<link rel="stylesheet" href="frontlogin/assets/css/main.css" />
+		
+	
+	<title>Login</title>
 	</head>
-
+	
 	<body>
-	<table align="center">
-		<form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" >
-			<tr>
-				<label>
-			<td>
-				Usuario:
-			</td>
-			<td>
-			<input id="usuario" name="usuario" type="text" ></div>
-			</label>
-			</td>
-			</tr>
-			<tr>
-			<td>
-			<div><label>Password:</label>
-			</td>
-			<td>
-			<input id="password" name="password" type="password">
-			</td>
-			</tr>
-			<tr>
-			<td></td>
-			<td>
-			<div><input name="login" type="submit" value="login"></div>
-			</td>
-			</tr>
-		</form>
+	<header id="header">
+	<h1><font color="#FF9933">Sof</font>traiN</h1>
+	<!--<p>sistema</p>-->
+	</header>
+		<form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" > 
+		Usuario:
+		<input id="usuario" name="usuario" type="text" >
+		Contraseña:
+		<input id="password" name="password" type="password"><br/>
+		<input name="login" type="submit" value="Ingresar">
 
-		<br />
-
+		</form> 
+		
+		
+		
 		<div style = "font-size:16px; color:#cc0000;"><?php echo isset($error) ? utf8_decode($error) : '' ; ?></div>
-	</table>
+		
+		<footer id="footer">
+		
+				<ul class="icons">
+					
+					<li><a href="#" class="icon fa-youtube"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon fa-facebook"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>softrain-srl.com</a></li>
+				</ul>
+
+			</footer>
+	<script src="frontlogin/assets/js/main.js"></script>
 	</body>
 </html>
-<?php
+<?php	
 	session_start();
-
+	
 	if(isset($_SESSION["usuario"])){
 		header("Location: controlador/Formulario.php");
 	}
-
+	
 	if(!empty($_POST))
 	{
 		$usuario = mysqli_real_escape_string($conexion,$_POST['usuario']);
 		$password = mysqli_real_escape_string($conexion,$_POST['password']);
 		$error = '';
-
+		
 		//$sha1_pass = sha1($password);
-
-		$sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND password = '$password'";
+		
+		$sql = "SELECT usuario, password FROM usuarios WHERE usuario = '$usuario' AND password = '$password'";
 		$result=$conexion->query($sql);
 		$rows = $result->num_rows;
-
+		
 		if($rows > 0) {
 			$row = $result->fetch_assoc();
 			$_SESSION['usuario'] = $row['usuario'];
-			$_SESSION['id'] = $row['id'];
-
+			$_SESSION['pass'] = $row['password'];
+				
 			header("location: Controlador/Formulario.php");
 			} else {
 			$error = "El nombre o contraseña son incorrectos";
